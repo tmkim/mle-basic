3ad8575d3e84d9184aacfeef400942b8d6c9a461
alchemynt <alchemynte@gmail.com>
2023-05-25 00:41:56 -0400

    reconfig atlas_uri

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ import { questionRouter } from "./question.routes";
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
  
-const { ATLAS_URI } = process.env;
+const ATLAS_URI = process.env.ATLAS_URI;
  
 if (!ATLAS_URI) {
    console.error("No ATLAS_URI environment variable has been defined in config.env");
@@ -17,6 +17,7 @@ if (!ATLAS_URI) {
  
 connectToDatabase(ATLAS_URI)
    .then(() => {
+       console.log('connecting to ATLAS_URI')
        const app = express();
        app.use(cors());
  
